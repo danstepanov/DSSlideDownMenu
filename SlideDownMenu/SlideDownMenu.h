f274ce1a8e596a8889c3051702c6e1331bc318cc
Dan Stepanov <danhstepanov@gmail.com>
2015-11-08 19:53:56 -0800

    Initial Commit

---
+++ SlideDownMenu/SlideDownMenu.h
@@ -0,0 +1,19 @@
+//
+//  SlideDownMenu.h
+//  SlideDownMenu
+//
+//  Created by Daniel Stepanov on 11/8/15.
+//  Copyright © 2015 Daniel Stepanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlideDownMenu.
+FOUNDATION_EXPORT double SlideDownMenuVersionNumber;
+
+//! Project version string for SlideDownMenu.
+FOUNDATION_EXPORT const unsigned char SlideDownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideDownMenu/PublicHeader.h>
+
+
